54a5b858b6becb2e33f306277325112fcf629677
Kirill Mehtiev <kirill.mehtiev@gmail.com>
2025-04-09 17:18:19 +0200

    test commit

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -33,9 +33,11 @@ public class Program
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+
         }
+        
+        app.UseSwagger();
+        app.UseSwaggerUI();
         
         app.UseHttpsRedirection();
         // app.UseAuthorization();
